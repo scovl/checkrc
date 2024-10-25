c24dd8c8e35fac9a37dfedc7faf8e9fe7642e389
Vitor Lobo <lobocode@gmail.com>
2024-10-25 11:18:33 -0300

    apply clang-format

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+
 #include "config.h"
 #include "validation_utils.h"
 
@@ -10,59 +11,61 @@
 int process_line(char* line, int line_number);
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s /path/to/rc.conf\n", argv[0]);
-        return EXIT_FAILURE;
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s /path/to/rc.conf\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  FILE* file = fopen(argv[1], "r");
+  if (!file) {
+    fprintf(stderr, "Failed to open file: %s\n", argv[1]);
+    perror("Error");
+    return EXIT_FAILURE;
+  }
+
+  char line[MAX_LINE_LENGTH];
+  int line_number = 0;
+  size_t error_count = 0;
+
+  while (fgets(line, sizeof(line), file)) {
+    line_number++;
+    if (!process_line(line, line_number)) {
+      error_count++;
     }
+  }
 
-    FILE* file = fopen(argv[1], "r");
-    if (!file) {
-        fprintf(stderr, "Failed to open file: %s\n", argv[1]);
-        perror("Error");
-        return EXIT_FAILURE;
-    }
+  fclose(file);
 
-    char line[MAX_LINE_LENGTH];
-    int line_number = 0;
-    size_t error_count = 0;
-
-    while (fgets(line, sizeof(line), file)) {
-        line_number++;
-        if (!process_line(line, line_number)) {
-            error_count++;
-        }
-    }
-
-    fclose(file);
-
-    if (error_count == 0) {
-        printf("Config check passed successfully.\n");
-        return EXIT_SUCCESS;
-    } else {
-        printf("Config check failed with %zu error(s).\n", error_count);
-        return EXIT_FAILURE;
-    }
+  if (error_count == 0) {
+    printf("Config check passed successfully.\n");
+    return EXIT_SUCCESS;
+  } else {
+    printf("Config check failed with %zu error(s).\n", error_count);
+    return EXIT_FAILURE;
+  }
 }
 
 int process_line(char* line, int line_number) {
-    char* temp_line = line;
-    while (*temp_line && isspace((unsigned char)*temp_line)) temp_line++;
-    if (*temp_line == '\0' || *temp_line == '#') return 1;
+  char* temp_line = line;
+  while (*temp_line && isspace((unsigned char)*temp_line)) temp_line++;
+  if (*temp_line == '\0' || *temp_line == '#') return 1;
 
-    char* delimiter = strchr(line, '=');
-    if (!delimiter) {
-        fprintf(stderr, "Warning: Line %d is not in 'key=value' format.\n", line_number);
-        return EXIT_SUCCESS;
-    }
+  char* delimiter = strchr(line, '=');
+  if (!delimiter) {
+    fprintf(stderr, "Warning: Line %d is not in 'key=value' format.\n",
+            line_number);
+    return EXIT_SUCCESS;
+  }
 
-    *delimiter = '\0';
-    char* key = trim_space(line);
-    char* value = trim_space(delimiter + 1);
+  *delimiter = '\0';
+  char* key = trim_space(line);
+  char* value = trim_space(delimiter + 1);
 
-    if (value && !validate_option(key, value)) {
-        printf("Error on line %d: '%s' is not a valid value for '%s'\n", line_number, value, key);
-        return EXIT_SUCCESS;
-    }
+  if (value && !validate_option(key, value)) {
+    printf("Error on line %d: '%s' is not a valid value for '%s'\n",
+           line_number, value, key);
+    return EXIT_SUCCESS;
+  }
 
-    return EXIT_FAILURE;
+  return EXIT_FAILURE;
 }
--- src/validation_utils.c
+++ src/validation_utils.c
@@ -1,53 +1,50 @@
-#include "validation_utils.h"
 #include <ctype.h>
 #include <string.h>
 
-char* trim_space(char* str) {
-    char* end;
+#include "validation_utils.h"
 
-    while (isspace((unsigned char)*str) || *str == '"') str++;
+char *trim_space(char *str) {
+  char *end;
+  while (isspace((unsigned char)*str) || *str == '"') str++;
+  if (*str == 0) return str;
+  end = str + strlen(str) - 1;
 
-    if (*str == 0)
-        return str;
+  while (end > str && (isspace((unsigned char)*end) || *end == '"')) end--;
 
-    end = str + strlen(str) - 1;
+  end[1] = '\0';
 
-    while (end > str && (isspace((unsigned char)*end) || *end == '"')) end--;
-
-    end[1] = '\0';
-
-    return str;
+  return str;
 }
 
-bool validate_option(const char* key, char* value) {
-    char* comment = strchr(value, '#');
-    if (comment) {
-        *comment = '\0';
-    }
+bool validate_option(const char *key, char *value) {
+  char *comment = strchr(value, '#');
+  if (comment) {
+    *comment = '\0';
+  }
 
-    value = trim_space(value);
+  value = trim_space(value);
 
-    for (size_t i = 0; i < sizeof(config_options) / sizeof(ConfigOption); i++) {
-        if (strcmp(config_options[i].name, key) == 0) {
-            switch (config_options[i].type) {
-                case TYPE_STRING:
-                    return true;
-                case TYPE_INT:
-                    for (size_t j = 0; value[j] != '\0'; j++) {
-                        if (!isdigit(value[j]) && !(value[j] == '-' && j == 0)) {
-                            return false;
-                        }
-                    }
-                    return true;
-                case TYPE_BOOL:
-                    for (int j = 0; config_options[i].valid_values[j] != NULL; j++) {
-                        if (strcasecmp(config_options[i].valid_values[j], value) == 0) {
-                            return true;
-                        }
-                    }
-                    return false;
+  for (size_t i = 0; i < sizeof(config_options) / sizeof(ConfigOption); i++) {
+    if (strcmp(config_options[i].name, key) == 0) {
+      switch (config_options[i].type) {
+        case TYPE_STRING:
+          return true;
+        case TYPE_INT:
+          for (size_t j = 0; value[j] != '\0'; j++) {
+            if (!isdigit(value[j]) && !(value[j] == '-' && j == 0)) {
+              return false;
             }
-        }
+          }
+          return true;
+        case TYPE_BOOL:
+          for (int j = 0; config_options[i].valid_values[j] != NULL; j++) {
+            if (strcasecmp(config_options[i].valid_values[j], value) == 0) {
+              return true;
+            }
+          }
+          return false;
+      }
     }
-    return true;
+  }
+  return true;
 }

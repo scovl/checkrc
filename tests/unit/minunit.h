33fe15712d6b9ba0ed1b92027f0379003ba37deb
Vitor Lobo Ramos <lobocode@gmail.com>
2025-02-14 12:22:21 -0300

    fix it

--- tests/unit/minunit.h
+++ tests/unit/minunit.h
@@ -4,10 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int tests_run = 0;
+
 #define MU_ASSERT(message, test) do { if (!(test)) return message; } while (0)
 #define MU_RUN_TEST(test) do { char *message = test(); tests_run++; \
                                if (message) return message; } while (0)
-extern int tests_run;
 
 /* Function pointers instead of direct declarations */
 static void (*test_setup)(void) = NULL;
